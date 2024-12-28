d473b4ff675fdd7184c05579687961e1d1451784
kakareally <milan.inn@gmail.com>
2024-12-28 22:43:02 +0800

    新增2个文件

---
+++ chapter1/chapter1_3.py
@@ -0,0 +1,11 @@
+print('''
+Hello David, I am Tom form England, Let's go to watch
+a movie together, "Hello world!"
+Okay, very good!
+''')
+
+print('Hello\nworld!')
+
+path = "C:\nnowhere"
+print(path)
+print(r'C:\nnowhere')
